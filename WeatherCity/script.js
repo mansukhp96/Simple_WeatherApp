8d12bc12cff9dc37e7d98f7b10f46c2538402cd7
mansukhp96 <33620662+mansukhp96@users.noreply.github.com>
2018-02-05 16:49:13 +0530

    Basic Bootstrap

--- WeatherCity/script.js
+++ WeatherCity/script.js
@@ -1,9 +1,20 @@
-
 var myApp = angular.module("weatherApp",[]);
-var details ;
-myApp.controller("weatherController", function($scope){
-    $scope.cityName = "";    
-      var apiKey = "a591de92adcf5e271f5f78169919386f";
-      var owu = "api.openweathermap.org/data/2.5/weather?q="+ cityName;
-      console.log(owu, 'res');
+myApp.controller("weatherController", function($scope,$http){
+  var cityName;
+  $scope.send = function(){
+    $scope.Name = cityName;
+    var apiKey = "a591de92adcf5e271f5f78169919386f";
+    var owu = "http://api.openweathermap.org/data/2.5/weather?q="+ $scope.cityName+ "&appid=" +apiKey;
+    //console.log(owu);
+    $http({
+    method: 'get', 
+    url: owu
+    }).then(function (response) {
+      //console.log(response);
+      $scope.temperature = response.data.main.temp;
+      var tempf = $scope.temperature - 273;
+      document.getElementById("city").innerHTML = $scope.cityName; 
+      document.getElementById("tempf").innerHTML = tempf+"Celsius";
+      });
+  }   
 });
